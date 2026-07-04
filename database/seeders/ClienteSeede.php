5180e30afa9c4e4db34286e7cd45b39661b6815e
Alex <cuellaralex314@gmail.com>
2026-07-04 14:52:29 -0300

    dvd

---
+++ database/seeders/ClienteSeede.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClienteSeede extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
