0e078f60576b6923804f54499015c773c15e453a
Alex <cuellaralex314@gmail.com>
2026-07-04 17:19:07 -0300

    df

--- database/migrations/2026_07_04_174751_create_orden_servicio2_table.php
+++ database/migrations/2026_07_04_174751_create_orden_servicio2_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orden_servicio', function (Blueprint $table) {
+        Schema::create('orden_servicio2', function (Blueprint $table) {
             $table->id();
             $table->foreignId('orden_trabajo_id')->constrained()->onDelete('cascade');
             $table->foreignId('servicio_id')->constrained()->onDelete('cascade');
