4f1d56cab0a182ca5b4948525ea8e471a002a5b8
Alex <cuellaralex314@gmail.com>
2026-07-04 15:58:11 -0300

    gn

--- app/Models/Servicio.php
+++ app/Models/Servicio.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Servicio extends Model
 {
-   use HasFactory;
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'precio_base'
+    ];
+
+    public function ordenes()
+    {
+        return $this->belongsToMany(
+            OrdenTrabajo::class,
+            'orden_servicio'
+        );
+    }
 }
