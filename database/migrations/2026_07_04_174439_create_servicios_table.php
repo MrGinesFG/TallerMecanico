3a517f96eb73f6d6ed18e73e667a3f34515441ba
Alex <cuellaralex314@gmail.com>
2026-07-04 16:00:09 -0300

    vv

--- database/migrations/2026_07_04_174439_create_servicios_table.php
+++ database/migrations/2026_07_04_174439_create_servicios_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('servicios', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio_base', 10, 2);
             $table->timestamps();
         });
     }
