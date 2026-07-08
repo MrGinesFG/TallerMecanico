e6a66c470203b267c39e3350a38be0e29dfebe65
Fabrizio Ginés <f.gines17@gmail.com>
2026-07-08 15:49:49 -0300

    Debug: Habilitar APP_DEBUG en config/app.php temporalmente

--- config/app.php
+++ config/app.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
